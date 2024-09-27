a3c3f288ebccecf02da887e0bea3af237d5b7a47
Liridon Gashi <doo.nii@hotmail.com>
2024-09-27 16:14:50 +0200

    Logo s

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,13 @@ function checkBox() {
 
 window.addEventListener("scroll", checkBox);
 checkBox();
+
+// toggle navbar
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const navbar = document.querySelector(".navbar");
+
+  hamburger.addEventListener("click", function () {
+    navbar.classList.toggle("active-nav");
+  });
+});
